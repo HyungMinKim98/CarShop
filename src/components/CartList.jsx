242b14358a3c535495300de3b77b503f60e41b3b
HyungMinKim98 <you@example.com>
2023-02-10 11:43:42 +0900

    css update

--- src/components/CartList.jsx
+++ src/components/CartList.jsx
@@ -14,7 +14,7 @@ const CartList=()=>{
             <h2 className="cart-list-title">장바구니</h2>
             {cartItem}
             <br/>
-            <hr/>
+            <hr className="hr"/>
             <div class="frame">
                 <i class="fal fa-money-check fa-lg" ></i>
                 <button class="custom-btn btn-12"><span>Click!</span><span>주문하기</span></button>
